4ffae1e451e27049a32caf867e0f33e048fa62d9
AmineNouriWebDev <macbookair@MacBook-Air-de-macbookair.local>
2024-06-23 12:53:29 +0100

    navbar styling & burger menu

--- book-store/src/components/header/Header.jsx
+++ book-store/src/components/header/Header.jsx
@@ -1,39 +1,19 @@
+import { useState } from "react";
 import "./header.css";
-const Header = ()=>{
-    return (
-        <header className="header">
-            <div className="header-top">
-                <div className="header-top-menu">
-                    <i className="bi bi-list"></i>
-                </div>
-                <div className="header-top-phone">
-                    <i className="bi bi-telephone-fill"></i>
-                    123-456-789
-                </div>
-                <div className="header-top-text">
-                    Welcome To Online Book Store
-                </div>
-                <div className="header-top-link">
-                    <i className="bi bi-person-fill"></i>
-                    Login
-                </div>
-            </div>
-            <div className="header-middle">
-                <div className="header-middle-logo">
-                    <b>Book</b>
-                    <i className="bi bi-book"></i>
-                    <b>Store</b>
-                </div>
-                <div className="header-middle-search-box">
-                    <input type="search" placeholder="Search in book store..." className="header-middle-search-input" />
-                    <i className="bi bi-search"></i>
-                </div>
-                <div className="header-middle-cart-wrapper">
-                    <i className="bi bi-cart2"></i>
-                </div>
-            </div>
-        </header>
-    )
+import HeaderMiddle from "./HeaderMiddle";
+import HeaderTop from "./HeaderTop";
+import Navbar from "./Navbar";
 
-}
+const Header = () => {
+    const [toggle, setToggle] = useState(false);
+
+    return (
+    <header className="header">
+        <HeaderTop toggle={toggle} setToggle={setToggle} />
+        <HeaderMiddle />
+        <Navbar setToggle={setToggle} toggle={toggle} />
+    </header>
+    );
+};
+
 export default Header;
