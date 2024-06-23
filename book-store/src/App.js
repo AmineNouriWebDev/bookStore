7f86b1ec6e49ff9b525e1af90308b60ad60cdacf
AmineNouriWebDev <macbookair@MacBook-Air-de-macbookair.local>
2024-06-23 19:11:47 +0100

    creaton book-slider

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import BookSlider from './components/bool-slider/BookSlider';
 import Header from './components/header/Header';
 import Services from './components/services/Services';
 import Slider from './components/slider/Slider';
-
+import {books} from "./data/books";
 function App() {
   return (
     <div>
     <Header />
     <Slider />
     <Services />
+    <BookSlider  data = {books}/>
     </div>
   );
 }
