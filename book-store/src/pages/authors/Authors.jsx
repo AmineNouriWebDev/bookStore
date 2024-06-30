ff2c1333bb0b6dcef6085debe17077f2a11a80b7
AmineNouriWebDev <macbookair@MacBook-Air-de-macbookair.local>
2024-06-30 17:15:17 +0100

    Creating Authors Page

--- book-store/src/pages/authors/Authors.jsx
+++ book-store/src/pages/authors/Authors.jsx
@@ -1,9 +1,24 @@
-
+import "./authors.css"
+import { useState } from "react";
+import { authors } from "../../data/authors";
 const Authors = () => {
+    const [search, setSearch] = useState("");
     return (
-        <h1>
-            Authors
-        </h1>
+        <section className="authors">
+            <div className="authors-search-wrapper">
+                <input value={search} onChange={e => setSearch(e.target.value)} type="search" placeholder="Search an author" />
+            </div>
+            <div className="authors-wrapper">
+                {authors
+                .filter(a => a.name.toLocaleLowerCase().includes(search))
+                .map((author) => 
+                    <div className="author"  key={author.id}>
+                        <img src={author.image} alt={author.name} className="author-img" />
+                        <h2 className="author-name">{author.name}</h2>
+                    </div>
+                    )}
+            </div>
+        </section>
     );
 }
 
