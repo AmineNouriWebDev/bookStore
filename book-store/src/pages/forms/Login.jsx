9b8df04f726ebd6e608e9388353392e40ce909f5
AmineNouriWebDev <nouri.webdev@gmail.com>
2024-07-01 15:03:26 +0100

    add register page

--- book-store/src/pages/forms/Login.jsx
+++ book-store/src/pages/forms/Login.jsx
@@ -7,22 +7,29 @@ import 'react-toastify/dist/ReactToastify.css';
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
     // form submit
     const formSubmitHandler = (e) => {
         e.preventDefault();
         if(email.trim()==="") return toast.error("Email is required!");
         if(password.trim()==="") return toast.error("Password is required!");
-        console.log(email, password)
+    
     }
-
+    // Show password handler
+    const showPasswordHandler = () =>{
+        setShowPassword(prev => !prev)
+    }
     return (
         <div className="form-wrapper">
             <ToastContainer theme="colored" />
             <h1 className="form-title"> Login to your account </h1>
             <form onSubmit={formSubmitHandler} className="form">
                 <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="Email:" />
-                <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+                <input value={password} onChange={e => setPassword(e.target.value)} type={showPassword ? "text" : "password"} placeholder="Password" />
+                {showPassword ? (<i onClick={showPasswordHandler} className="bi bi-eye-slash-fill show-password-icon"></i> ) : (<i onClick={showPasswordHandler} className="bi bi-eye-fill show-password-icon"></i>)  }
+                
+                
                 <button className="form-btn">Login</button>
             </form>
             <div className="form-footer">
