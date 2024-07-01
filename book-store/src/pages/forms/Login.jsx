d248c1f6d368b6055dd1ac4a58b7b57f7152980f
AmineNouriWebDev <nouri.webdev@gmail.com>
2024-07-01 14:29:43 +0100

    add login page
    (form and css)

--- book-store/src/pages/forms/Login.jsx
+++ book-store/src/pages/forms/Login.jsx
@@ -1,7 +1,38 @@
+import "./forms.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // form submit
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+        if(email.trim()==="") return toast.error("Email is required!");
+        if(password.trim()==="") return toast.error("Password is required!");
+        console.log(email, password)
+    }
+
     return (
-        <h1>Login</h1>
+        <div className="form-wrapper">
+            <ToastContainer theme="colored" />
+            <h1 className="form-title"> Login to your account </h1>
+            <form onSubmit={formSubmitHandler} className="form">
+                <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="Email:" />
+                <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+                <button className="form-btn">Login</button>
+            </form>
+            <div className="form-footer">
+                Don't have an account?
+                <Link to="/register" className="form-link">
+                {" "}Register
+                </Link>
+
+            </div>
+        </div>
     );
 }
 
